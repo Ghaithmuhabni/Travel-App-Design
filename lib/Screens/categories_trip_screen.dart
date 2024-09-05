f736bc16bac142b888e87286c3b590c6f532c21d
Ghaithmuhabni <muhbanifuture@gmail.com>
2024-09-05 12:03:15 +0300

    Navigate between program pages and open and close application screens

---
+++ lib/Screens/categories_trip_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class categoriesTripScreen extends StatelessWidget {
+  const categoriesTripScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Trips Page"),
+      ),
+      body: Center(child: Text("This summer's itinerary list")),
+    );
+  }
+}
--- lib/Widgets/category_item.dart
+++ lib/Widgets/category_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import '../Screens/categories_trip_screen.dart';
 
 class categoryItem extends StatelessWidget {
   final String title;
@@ -7,31 +8,41 @@ class categoryItem extends StatelessWidget {
 
   const categoryItem(this.title, this.imageUrl);
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx)
+        .push(MaterialPageRoute(builder: (c) => categoriesTripScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(15),
-          child: Image.network(
-            imageUrl,
-            height: 290,
-            fit: BoxFit.cover,
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(10),
-          alignment: Alignment.center,
-          child: Text(
-            title,
-            style: TextStyle(fontSize: 28, color: Colors.white),
-          ),
-          decoration: BoxDecoration(
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Stack(
+        children: [
+          ClipRRect(
             borderRadius: BorderRadius.circular(15),
-            color: Colors.black.withOpacity(0.4),
+            child: Image.network(
+              imageUrl,
+              height: 290,
+              fit: BoxFit.cover,
+            ),
           ),
-        )
-      ],
+          Container(
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: Text(
+              title,
+              style: TextStyle(fontSize: 28, color: Colors.white),
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.black.withOpacity(0.4),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
