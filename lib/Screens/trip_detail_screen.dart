e62b78579835eec97cb2d102991995cf5d569630
Ghaithmuhabni <muhbanifuture@gmail.com>
2024-09-15 10:01:37 +0300

    continue the trip detail screen disgn

--- lib/Screens/trip_detail_screen.dart
+++ lib/Screens/trip_detail_screen.dart
@@ -4,6 +4,32 @@ import 'package:flutter_application_1/app_data.dart';
 class tripDetailScreen extends StatelessWidget {
   static const screenRoute = '/trip-detail';
 
+  Widget sectionTitle(
+    String title,
+  ) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+      alignment: Alignment.centerLeft,
+      child: Text(
+        title,
+        style: const TextStyle(
+            fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blue),
+      ),
+    );
+  }
+
+  Widget buildListViewContainer(Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(15)),
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      height: 250,
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final tripId = ModalRoute.of(context)?.settings.arguments as String;
@@ -14,50 +40,66 @@ class tripDetailScreen extends StatelessWidget {
         title: Text("${selectedTrip.title}"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(
-              selectedTrip.imageUrl,
-              fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedTrip.imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Activities",
-              style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue),
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(15)),
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            height: 250,
-            child: ListView.builder(
-              itemCount: selectedTrip.activities.length,
-              itemBuilder: (context, index) => Card(
-                elevation: 0.3,
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  child: Text(
-                    selectedTrip.activities[index],
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            sectionTitle("Activities"),
+            buildListViewContainer(
+              ListView.builder(
+                itemCount: selectedTrip.activities.length,
+                itemBuilder: (context, index) => Card(
+                  elevation: 0.3,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Text(
+                      selectedTrip.activities[index],
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
                   ),
                 ),
               ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 10,
+            ),
+            sectionTitle("Daily program"),
+            buildListViewContainer(ListView.builder(
+                itemCount: selectedTrip.program.length,
+                itemBuilder: (ctx, index) => Column(
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            child: Text(
+                              "Day ${index + 1}",
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ),
+                          title: Text(
+                            selectedTrip.program[index],
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                        Divider()
+                      ],
+                    ))),
+            SizedBox(
+              height: 75,
+            )
+          ],
+        ),
       ),
     );
   }
